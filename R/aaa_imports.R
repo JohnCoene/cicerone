00c9e1e812da188d0d19cb71d69102573d8985c5
Stephen Holsenbeck <stephen@virgalabs.io>
2022-01-21 22:51:22 -0500

    Make internal `%||%`, add `prep_element`

--- R/aaa_imports.R
+++
@@ -1,3 +0,0 @@
-#' Re-imports
-#' @importFrom rlang `%||%`
-`%||%` <- rlang::`%||%`
--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,12 @@
 generate_id <- function() {
   paste0(sample(letters, 26), collapse = "")
 }
+
+prep_element <- function (el) if (!grepl("^\\.", el) && !grepl("^\\#", el)) paste0("#", el) else el
+
+`%||%` <- function(x, y) {
+  if (is.null(x))
+    y
+  else x
+}
+
